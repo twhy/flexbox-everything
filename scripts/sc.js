9ccf9a89473b100133a4d3f19ab260881539a08e
Tom Wang <tom.wanghaiyi@gmail.com>
2017-06-21 17:28:35 +0800

    JavaScript-less nav toggle

--- scripts/sc.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-  var $header = document.querySelector('.header');
-  var $toggle = document.querySelector('.nav-toggle');
-  $toggle.addEventListener('click', function(e) {
-    e.preventDefault();
-    e.currentTarget.classList.toggle('open');
-  });
-});
